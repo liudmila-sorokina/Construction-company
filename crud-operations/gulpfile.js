d3aae1d335bfb3ff4633d54a2a6283862e380dd6
Люся <lusorokina89@gmail.com>
2023-02-21 19:55:14 +0400

    Does home page

--- crud-operations/gulpfile.js
+++ crud-operations/gulpfile.js
@@ -3,6 +3,10 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
+gulp.task('watch', function () {
+  gulp.watch('./scss/**/*.scss', gulp.series('default'))
+})
+
 function defaultTask() {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
