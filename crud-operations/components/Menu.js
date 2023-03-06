05aa5304288868a66ffb3f243afee753ed51653b
Люся <lusorokina89@gmail.com>
2023-03-06 19:10:39 +0400

    Adds payment section

--- crud-operations/components/Menu.js
+++ crud-operations/components/Menu.js
@@ -1,14 +1,42 @@
 import React from "react";
 import MenuItem from "./MenuItem";
 
+const itemsArray = [
+  {
+    title: "Home",
+    imgpath: "./svg/hause-crud.svg",
+    url: "/home"
+  },
+  {
+    title: "Course",
+    imgpath: "./svg/bookmark-crud.svg",
+    url: "/course"
+  },
+  {
+    title: "Students",
+    imgpath: "./svg/Hat-crud.svg",
+    url: "/students"
+  },
+  {
+    title: "Payment",
+    imgpath: "./svg/Dollar-crud.svg",
+    url: "/payment"
+  },
+  {
+    title: "Report",
+    imgpath: "./svg/page-crud.svg",
+    url: "/report"
+  },
+  {
+    title: "Settings",
+    imgpath: "./svg/mixing-console-crud.svg",
+    url: "/settings"
+  }
+]
+
 const Menu = () => {
   return (<ul className="sidebar__list">
-    <MenuItem title="Home" imgpath="./svg/hause-crud.svg" url="/home" />
-    <MenuItem title="Course" imgpath="./svg/bookmark-crud.svg" url="/course" />
-    <MenuItem title="Students" imgpath="./svg/Hat-crud.svg" url="/students" />
-    <MenuItem title="Payment" imgpath="./svg/Dollar-crud.svg" url="/payment" />
-    <MenuItem title="Report" imgpath="./svg/page-crud.svg" url="/report" />
-    <MenuItem title="Settings" imgpath="./svg/mixing-console-crud.svg" url="/settings" />
+    {itemsArray.map((item) => <MenuItem title={item.title} imgpath={item.imgpath} url={item.url} key={item.title} />)}
   </ul>);
 };
 
--- crud-operations/components/containers/Payment.js
+++ crud-operations/components/containers/Payment.js
@@ -1,5 +1,41 @@
 import React from "react";
+import InformSearch from "../InformSearch";
+import PaymentsItem from "../PaymentsItem";
 
-const Payment = () => <h1>Payment</h1>;
+const Payment = () => {
+  return (
+    <section className="payments-list">
+<InformSearch />
+      <div className="students-list__search">
+        <h2 className="students-list__title">
+        Payment Details
+        </h2>
+        <div className="students-list__buttons-container">
+          <button className="students-list__filter">
+            <img src="./svg/arrows-up-down-crud.svg" alt="arrows up down" />
+          </button>
+        </div>
+      </div>
+      <table className="payments-list__table">
+        <thead>
+          <tr className="payments-list__table-row">
+            <th className="payments-list__table-head">Name</th>
+            <th className="payments-list__table-head">Payment Schedule</th>
+            <th className="payments-list__table-head">Bill Number</th>
+            <th className="payments-list__table-head">Amount Paid</th>
+            <th className="payments-list__table-head">Balance amount</th>
+            <th className="payments-list__table-head">Date</th>
+            <th className="payments-list__table-head"></th>
+          </tr>
+        </thead>
+        <tbody>
+        <PaymentsItem name="Karthi" paymentSchedule="First" bill="00012223" amountPaid="INR 35,000" balance="INR 55,000" data="08-Dec, 2021" />
+        <PaymentsItem name="Karthi" paymentSchedule="First" bill="00012223" amountPaid="INR 35,000" balance="INR 55,000" data="08-Dec, 2021" />
+        <PaymentsItem name="Karthi" paymentSchedule="First" bill="00012223" amountPaid="INR 35,000" balance="INR 55,000" data="08-Dec, 2021" />
+        </tbody>
+      </table>
+    </section>
+  )
+};
 
 export default Payment;
