e5639b2c0ffd0003e1ea0d027eff806c04bf843a
Люся <lusorokina89@gmail.com>
2023-03-09 22:41:42 +0400

    Adds backend in courses section

--- crud-operations/components/containers/Course.js
+++ crud-operations/components/containers/Course.js
@@ -1,9 +1,64 @@
-import React from "react";
+//указываем, что в этот модуль импортируем (в этом модуле он будет доступен) реакт, юзэффектс, юзстате
+
+import React, {useEffect, useState} from "react";
 import InformSearch from "../InformSearch";
 import CoursesItem from "../CoursesItem";
 
+//импортируем в этот модуль библиотеку аксиос, которая поможет потом нам передать запрос бекэнду
+
+import axios from "axios";
+
+//присваиваем переменной конфиг информацию о запросе на бекэнд (то есть то, что мы будем перерсылать на бекэнд: метод, урл-адрес, хедерсы)
+
+const config = {
+  method: 'get',
+  url: 'https://users-e87a.restdb.io/rest/courses',
+  headers: {
+    'Content-Type': 'application/json',
+    'x-apikey': '63e26aaf478852088da67e61'
+  }
+};
+
 const Course = () => {
+
+  //деструктурирующее присваивание. 
+  //стейт это карман, куда мы сохраняем полученные с бекэнда данные это переменная. Сэтстейт нужен, чтобы присваивать что-то стейту.
+  
+
+  const [state, setState] = useState([]) // я верну массив из 2х элементов. Этот метод - сеттер, его противоположность - геттер - это тот, который только возвращает любую переменную
+  
+  // function useState(arg) {
+  //   let internalState = arg
+  //   const modifyState = (newState) => internalState = newState
+
+  //   return [internalState, modifyState]
+  // }
+
+  // let pepe
+  // let pepe = {a: 1, b: 2}
+//функция, которая на вход принимает другую функцию 
+
+useEffect(() => {
+
+  //передаю аксиусу данные из конфига, чтобы аксиос сделал из них http запрос и отправил на бекэнд
+
+  axios(config)
+
+  //если данные с бекэнда загрузятся, то должно выполнится зэн - это как иф и тогда изменится стейт
+  //стрелочная ананимная функция, не присваиваем ее переменной, так как она сразу идет в работу
+
+  .then((respons) => setState(respons.data))
+
+  //если произойдет ошибка, данные с бекэнда не загрузятся, вывести в консоль ошибку
+
+  .catch((error) => (console.log(error)))
+
+  //пустые квадратные скобки - это условие, когда юзЭффект вызовет первый аргумент. Первый аргумент - это функция.
+}, [])
+
   return (
+//jsх - это код, похожий по виду на html. Чтобы вставить сюда js, надо использовать фигурные скобочки.
+
     <section className="courses-list">
       <InformSearch />
       <div className="students-list__search">
@@ -26,9 +81,20 @@ const Course = () => {
           </tr>
         </thead>
         <tbody>
+
+         {/* использую метод мап (похоже на forEach), которое позволит пройтись по всем элементам массива, присвоенного стате, и изменить их.
+         присваиваем пропсам компонента CoursesItem(через математический знак равно) значения, берущиеся из данных, присланных с бекэнда
+         пишем в фигурных скобках, так как это js, вставленный в react */}
+          {state.map((course) => <CoursesItem 
+          title={course.title}
+          description={`${course.description.substr(0, 20)}...`}
+          hours={course.how_many_hours}
+          />)}
+
+
+          {/*<CoursesItem title="Maths" description="deep course" hours="130h" />
           <CoursesItem title="Maths" description="deep course" hours="130h" />
-          <CoursesItem title="Maths" description="deep course" hours="130h" />
-          <CoursesItem title="Maths" description="deep course" hours="130h" />
+  <CoursesItem title="Maths" description="deep course" hours="130h" />*/}
         </tbody>
       </table>
     </section>
